1ae42cda346aedef67091d0d5b22097870c3c503
DinhHuySang <muaxuan0320@gmail.com>
2021-04-07 16:13:16 +0700

    .ssh to run build_vocabulary.py

---
+++ build_vocab.sh
@@ -0,0 +1,4 @@
+python3 bert-vocab-builder/subword_builder.py \ 
+--corpus_filepattern "/media/leo/967C7A9E7C7A7937/linux/data_for_Language_Model/backup/k/try.txt" \ 
+--output_filename "/media/leo/967C7A9E7C7A7937/linux/data_for_Language_Model/backup/vocab/vocab2.txt" \ 
+--max_subtoken_length 35000
